ebe6923a0132784fd3297a06e809ade2d4c63e6d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-24 19:20:30 +0900

    feat: Add test for buildEnd()

--- tests/OGImagePlugin.test.ts
+++ tests/OGImagePlugin.test.ts
@@ -32,4 +32,16 @@ describe("OgImagePlugin", () => {
       ]);
     });
   });
+
+  describe("buildEnd", () => {
+    it("should generate and save images for all markdown files", async () => {
+      const siteConfig = {
+        outDir: "./tests/bin",
+      };
+
+      ogImagePlugin.mdFiles = [{ path: "/test-page.png", title: "Test Page" }];
+
+      await ogImagePlugin.buildEnd(siteConfig);
+    });
+  });
 });
