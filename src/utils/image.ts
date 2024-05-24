81b25bd16733b0c32c414dd4835c390c24657e50
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-24 18:39:15 +0900

    feat: Convert SVG to PNG using sharp

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,9 +1,10 @@
 import DefaultTemplate from "../components/default";
 import { MdFile } from "@/types";
 import satori, { SatoriOptions } from "satori";
+import sharp from "sharp";
 
 /**
- * Asynchronously generates an image for a given file.
+ * Generates a PNG image from a React element using Satori rendering engine.
  *
  * @param {Object} file - The markdown file object containing the file path and title.
  * @param {Object} options - Additional options for satori.
@@ -15,7 +16,10 @@ export const generateImage = async (file: MdFile, options: SatoriOptions) => {
 
     // Render the React element into SVG using the Satori rendering engine.
     const svg = await satori(element, options);
-    console.log(svg);
+
+    // Convert the SVG string to PNG Buffer using Sharp library
+    const pngBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
+    console.log(pngBuffer);
   } catch (error) {
     // Log an error message if an exception occurs during the image generation process.
     console.error(`Error processing file ${file.path}:`, error);
